201e320114386241628d9419e6ebe0201a8e6fa1
Riley Martin <riley.willow@gmail.com>
2021-02-07 10:37:49 -0700

    Complete iteration 1

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return `Delicious ${food}`
+}
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name,
+    price,
+    type,
+  };
+  return menuItem;
+}
+
+function addIngredients(newIngredient, ingredientArray) {
+    ingredientArray;
+    if (ingredientArray.includes(newIngredient)) {
+      return ingredientArray;
+    } else {
+      ingredientArray.push(newIngredient);
+      return ingredientArray;
+    }
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(price) {
+  return price * .9
 
 }
 
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+   title,
+   ingredients,
+   type,
+ };
+ return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
