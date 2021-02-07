921934d0cd7eb0da8c9fbce5480cdab8486eb8a7
Riley Martin <riley.willow@gmail.com>
2021-02-07 10:38:50 -0700

    Complete iteration 2

--- src/order.js
+++ src/order.js
@@ -1,7 +1,48 @@
 
+function takeOrder(newOrder, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(newOrder);
+  } else {
+    return deliveryOrders;
+  }
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  return deliveryOrders.splice([orderNum - 1], 1);
+}
+
+//
+// function listItems(deliveryOrders) {
+//   var list = deliveryOrders[0].slice(2);
+//   for (var i = 1; i < deliveryOrders.length; i++) {
+//     list.push(deliveryOrders[i].slice(2));
+//   }
+//   return list;
+// }
+
+
+function listItems(deliveryOrders) {
+  // for (var i = 0; i < deliveryOrders.length; i++) {
+    var list = [];
+    list.push(deliveryOrders[0].item);
+    list.push(deliveryOrders[1].item);
+    list.push(deliveryOrders[2].item);
+  // }
+  return `${list[0]}, ${list[1]}, ${list[2]}`
+}
+
+function searchOrder(deliveryOrders, food) {
+  if (deliveryOrders[0].item === food || deliveryOrders[1].item === food) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
