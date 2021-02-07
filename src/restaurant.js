dca41872b7590d61eff840918eb0abf9656fc742
Riley Martin <riley.willow@gmail.com>
2021-02-07 10:39:18 -0700

    Complete iteration 3

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,52 @@
 
 
+function createRestaurant(name) {
+  var restaurant = {
+    name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+  return restaurant
+}
+
+
+function addMenuItem(restaurant, item) {
+  if (restaurant.menus.breakfast.includes(item) || restaurant.menus.lunch.includes(item) || restaurant.menus.dinner.includes(item)) {
+    return restaurant;
+  } else {
+    if (item.type === "breakfast") {
+      restaurant.menus.breakfast.push(item);
+    }
+    if (item.type === "lunch") {
+      restaurant.menus.lunch.push(item);
+    }
+    if (item.type === "dinner") {
+      restaurant.menus.dinner.push(item);
+    }
+    return restaurant;
+  }
+}
+
+function removeMenuItem(restaurant, itemName, type) {
+//check the given menu for an item containing
+//the itemName
+//remove the item with that name from the appropriate menu
+  for (var i = 0; i < restaurant.menus[type].length; i++) {
+    if (restaurant.menus[type][i].name === itemName) {
+      restaurant.menus[type].splice([i]);
+      return `No one is eating our ${itemName} - it has been removed from the ${type} menu!`
+    }
+  }
+      return `Sorry, we don't sell ${itemName}, try adding a new recipe!`
+}
+
+
+
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
